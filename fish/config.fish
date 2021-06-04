808b500ce1a649c489a09fb0a19ab9c8db63cfb3
Jason Peddle <jason@typh.com>
2021-06-04 03:15:56 -0400

    Scripted auto-commit on change (2021-06-04 03:15:56) by gitwatch.sh

--- fish/config.fish
+++ fish/config.fish
@@ -50,9 +50,6 @@ set -x FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS
   --layout=reverse
   --no-info
   --border
-  --color=dark
-  --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f
-  --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
   --preview 'bat --color=always --style=header,grid --line-range :300 {}'
 "
 set -x FZF_DEFAULT_COMMAND "fd --type file"
