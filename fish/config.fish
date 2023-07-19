506ef8b75eb60b08ddbcaf1dbd41e59cbc4a85f2
Jason Peddle <jason@typh.com>
2023-07-19 19:50:04 -0400

    Scripted auto-commit on change (2023-07-19 19:50:04) by gitwatch.sh

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,7 @@ set -x fish_term24bit 1
 function execute_fuck_or_search_history
     if test -z (commandline)
         fuck
+        commandline -f repaint
     else
         commandline -f history-search-backward
     end
